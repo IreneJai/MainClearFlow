10153f5b562c41f15e280957c9e4d25392302414
IreneJai <122608089+IreneJai@users.noreply.github.com>
2024-05-05 11:20:00 +0800

    Add files via upload

---
+++ ImgHover.js
@@ -0,0 +1,11 @@
+
+function bigImg(x) {
+    x.style.height = "260px";
+    x.style.width = "280px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "220px";
+  }
+  
